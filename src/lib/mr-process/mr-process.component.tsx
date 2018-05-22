cde0457ff69051bd34c5e80a50f2e2b1626d9708
amily4555 <amily4555@gmail.com>
2018-05-22 12:35:12 +0800

    0.0.5 -> 0.1.24-b6

--- src/lib/mr-process/mr-process.component.tsx
+++ src/lib/mr-process/mr-process.component.tsx
@@ -7,12 +7,14 @@
  *
  * @update mizi.lin@v0.1.23.20180521
  * ::=> 修改nodataComponent 取值错误
+ * ::=> 证实在tsx可以使用import style 文件了``
+ *
  */
 
 import * as React from 'react';
 import * as mu from 'mzmu';
 import * as _ from 'lodash';
-import './mr-process.less';
+import '../assets/styles/mr-process.less';
 import {MrIf, MrElse, MrFill, MrCol, MrAutoBind, MrReq, MrPanel, MrComponent, MrServices} from '..';
 import MrLoader from './mr-loader.component';
 import NoDateComponent from './nodate.component';
